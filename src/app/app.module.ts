84e7b61cdd0bdb64dc10d569e13ef16e674fea51
Abhishek Kumar Jha <abhishek.jha8387@gmail.com>
2024-03-12 11:35:09 +0530

    Backend Setup for dashborad using Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {AngularFireModule} from '@angular/fire/compat';
+import {AngularFirestore, AngularFirestoreModule} from '@angular/fire/compat/firestore'
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layouts/header/header.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-
+import { observeInsideAngular } from '@angular/fire';
+import { environment } from '../environments/environment.development';
+import { from } from 'rxjs';
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +21,9 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+   AngularFireModule.initializeApp(environment.firebaseConfig),
+   AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,11 @@
 export const environment = {
-    production : false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAVM4euUPvgUQX552smyq3PxP8CKeW5t2E',
+    authDomain: 'ang-blog-app-54d15.firebaseapp.com',
+    projectId: 'ang-blog-app-54d15',
+    storageBucket: 'ang-blog-app-54d15.appspot.com',
+    messagingSenderId: '749829180474',
+    appId: '1:749829180474:web:b8ed91d58e14a2675c3903',
+  },
 };
